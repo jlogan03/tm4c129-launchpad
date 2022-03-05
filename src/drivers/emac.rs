8b86257e05972ed27972feec3d3ee51357cd068e
James Logan <jlogan@Jamess-MacBook-Pro.local>
2022-03-05 14:10:37 -0500

    WIP to add driver for EMAC peripheral

--- src/drivers/emac.rs
+++ src/drivers/emac.rs
@@ -1,1 +1,15 @@
 //! Drivers for TM4C129's EMAC/PHY peripheral
+use tm4c129x_hal::tm4c129x::EMAC0;
+
+
+/// Get preprogrammed MAC address from ROM
+pub fn get_rom_macaddr(emac: &EMAC0) -> [u8; 6] {
+    let mut addr = [0_u8; 6];
+    let ah = emac.addr0h.read().bits();
+
+    addr
+}
+
+// pub fn cfg() -> () {
+    
+// }

efb1672da40dc9840d2a6be8870dfa888d15abd6
James Logan <jlogan@Jamess-MacBook-Pro.local>
2022-03-18 18:14:26 -0400

    continuing WIP to implement EMACDriver

--- src/drivers/emac.rs
+++ src/drivers/emac.rs
@@ -1,5 +1,8 @@
 //! Drivers for TM4C129's EMAC/PHY media access control peripherals
-use tm4c129x_hal::{tm4c129x::{EMAC0, FLASH_CTRL}, sysctl::PllOutputFrequency};
+use tm4c129x_hal::{
+    sysctl::PllOutputFrequency,
+    tm4c129x::{EMAC0, FLASH_CTRL},
+};
 
 /// Get preprogrammed MAC address from ROM
 pub fn get_rom_macaddr(flash: &FLASH_CTRL) -> [u8; 6] {
@@ -10,14 +13,7 @@ pub fn get_rom_macaddr(flash: &FLASH_CTRL) -> [u8; 6] {
 
     // Only the least-significant 3 bytes of each register are parts of the address,
     // and they're stored in reverse order
-    let addr: [u8; 6] = [
-        addr0[3],
-        addr0[2],
-        addr0[1],
-        addr1[3],
-        addr1[2],
-        addr1[1], 
-    ];
+    let addr: [u8; 6] = [addr0[3], addr0[2], addr0[1], addr1[3], addr1[2], addr1[1]];
 
     addr
 }
@@ -62,7 +58,7 @@ pub fn emac_init(emac: &EMAC0) {
             .set_bit()
             .ucgf()
             .set_bit()
-    });  // Mask all rx interrupts
+    }); // Mask all rx interrupts
     emac.mmctxim.modify(|_, w| {
         w.gbf()
             .set_bit()
@@ -72,7 +68,7 @@ pub fn emac_init(emac: &EMAC0) {
             .set_bit()
             .scollgf()
             .set_bit()
-    });  // Mask all tx interrupts
+    }); // Mask all tx interrupts
 }
 
 /// Configure EMAC (must run phy_cfg, reset, then emac_init, then reset, then run emac_cfg)
@@ -84,91 +80,165 @@ pub fn emac_cfg(emac: &EMAC0) {
     emac.cfg.modify(|_, w| w.ipc().set_bit()); // Checksum offload
     emac.cfg.modify(|_, w| w.prelen()._7()); // 7-byte preamble
     emac.cfg.modify(|_, w| w.ifg()._96()); // 96-bit iter-frame gap
-    emac.cfg.modify(|_, w| w.bl()._1024());  // 1024-bit back-off limit
-    emac.cfg.modify(|_, w| w.ps().set_bit());  // Set port select (req. for MII/RMII PHY interface)
+    emac.cfg.modify(|_, w| w.bl()._1024()); // 1024-bit back-off limit
+    emac.cfg.modify(|_, w| w.ps().set_bit()); // Set port select (req. for MII/RMII PHY interface)
 
     // Set memory controller op mode
-    emac.dmaopmode.modify(|_, w| w.rsf().set_bit());  // RX store-and-forward
-    emac.dmaopmode.modify(|_, w| w.tsf().set_bit());  // TX store-and-forward 
-    emac.dmaopmode.modify(|_, w| w.ttc()._32());  // TX threshold
-    emac.dmaopmode.modify(|_, w| w.rtc()._32());  // RX threshold
+    emac.dmaopmode.modify(|_, w| w.rsf().set_bit()); // RX store-and-forward
+    emac.dmaopmode.modify(|_, w| w.tsf().set_bit()); // TX store-and-forward
+    emac.dmaopmode.modify(|_, w| w.ttc()._32()); // TX threshold
+    emac.dmaopmode.modify(|_, w| w.rtc()._32()); // RX threshold
 }
 
-
 /// Configuration & TX/RX for EMAC0 peripheral using internal PHY
+/// 
+/// Assumes speed is 100 base T, using internal PHY, PHY uses MDIX and autonegotiation, 
+/// 8-word descriptor size, mmc interrupts all masked, full duplex mode
 pub struct EMACDriver {
     // EMAC
     emac: EMAC0,
     system_clk_freq: PllOutputFrequency,
     src_macaddr: [u8; 6],
-    speed: Speed,
     checksum_offload: bool,
     preamble_length: PreambleLength,
     interframe_gap: InterFrameGap,
     backoff_limit: BackOffLimit,
     rx_store_fwd: bool,
     tx_store_fwd: bool,
-    full_duplex: bool,
     // EPHY
-    phy_mdix: bool,
-    phy_autonegotiate: bool,
+    // phy_mdix: bool,
+    // phy_autonegotiate: bool,
     // Direct Memory Access controller
     tx_thresh: TXThresholdDMA,
     rx_thresh: RXThresholdDMA,
     rx_burst_size: BurstSizeDMA,
     tx_burst_size: BurstSizeDMA,
     // RX/TX structures
-    
+    // rx_descriptor
 }
 
 impl EMACDriver {
     /// Configure PHY
-    /// 
+    ///
     /// This must be run before resetting phy, then resetting emac, then doing cfg_emac!!
-    /// 
+    ///
     /// The settings done here are not cleared by reset & must be latched with resets in the proper order
-    fn cfg_phy(&self) {
+    fn cfg_ephy(&self) {
         self.emac.pc.modify(|_, w| w.phyext().clear_bit()); // Use internal PHY (disable external)
-
-        match self.phy_mdix {
-            true => self.emac.pc.modify(|_, w| w.mdixen().set_bit()), // Enable MDIX
-            false => self.emac.pc.modify(|_, w| w.mdixen().clear_bit()), // Disable MDIX
-        }
-        
+        self.emac.pc.modify(|_, w| w.mdixen().set_bit()); // Enable MDIX
         self.emac.pc.modify(|_, w| w.anen().set_bit()); // Enable autonegotiation
         self.emac.cfg.modify(|_, w| w.fes().set_bit()); // Speed 100 base T
         self.emac.cfg.modify(|_, w| w.dupm().set_bit()); // Duplex mode full
     }
 
-    async fn transmit(data: &[u8]) {
+    /// Configure EMAC
+    ///
+    /// This must be done in a specific order - see docs for cfg_ephy
+    fn cfg_emac(&self) {
+        // Set up direct memory access
 
+        // Burst transfer limits
+        // Just using the values the mfr uses here - not sure what the tradeoffs are
+        // Pretty sure this is only unsafe due to old-style conversion
+        // to 5-bit integer; should be able to update upstream calc to be safe
+        let rxburst = match self.rx_burst_size {
+            // No 8x multiplier needed
+            BurstSizeDMA::_1 => 1,
+            BurstSizeDMA::_2 => 2,
+            BurstSizeDMA::_4 => 4,
+            BurstSizeDMA::_8 => 8,
+            BurstSizeDMA::_16 => 16,
+            BurstSizeDMA::_32 => 32,
+        }; // RX DMA controller max memory transfer size in words
+        let txburst = match self.rx_burst_size {
+            // No 8x multiplier needed
+            BurstSizeDMA::_1 => 1,
+            BurstSizeDMA::_2 => 2,
+            BurstSizeDMA::_4 => 4,
+            BurstSizeDMA::_8 => 8,
+            BurstSizeDMA::_16 => 16,
+            BurstSizeDMA::_32 => 32,
+        }; // If these are different sizes, unset fixed burst
+        // Set flag for whether programmable burst limits are the same for RX and TX
+        match rxburst == txburst {
+            true => self.emac.dmabusmod.modify(|_, w| w.usp().clear_bit()), // RX and TX burst limits are the same
+            false => self.emac.dmabusmod.modify(|_, w| w.usp().set_bit()) // RX and TX burst limits are different
+        }
+        // Set actual programmable burst limits
+        unsafe {
+            self.emac.dmabusmod.modify(|_, w| w.rpbl().bits(rxburst));
+            self.emac.dmabusmod.modify(|_, w| w.pbl().bits(txburst));
+        };
+
+        // Set size of descriptors (TX/RX software buffer unit)
+        // Using 8-word size by default here to reduce configuration cases - it's a fine number
+        self.emac.dmabusmod.modify(|_, w| w.atds().set_bit()); // Alternate descriptor size
+
+        // MII (communication between EMAC and EPHY)
+        self.emac.miiaddr.modify(|_, w| w.cr()._100_150()); // Set clock divider to match system
+
+        // Disable interrupts (set interrupt masks)
+        self.emac.mmcrxim.modify(|_, w| {
+            w.algnerr()
+                .set_bit()
+                .crcerr()
+                .set_bit()
+                .gbf()
+                .set_bit()
+                .ucgf()
+                .set_bit()
+        }); // Mask all rx interrupts
+        self.emac.mmctxim.modify(|_, w| {
+            w.gbf()
+                .set_bit()
+                .mcollgf()
+                .set_bit()
+                .octcnt()
+                .set_bit()
+                .scollgf()
+                .set_bit()
+        }); // Mask all tx interrupts
+
+        // Setup up EMAC transmission behavior
+        self.emac.cfg.modify(|_, w| w.dupm().set_bit()); // Full duplex mode (reassert after reset)
+        self.emac.cfg.modify(|_, w| w.ipc().set_bit()); // Checksum offload
+        self.emac.cfg.modify(|_, w| w.prelen()._7()); // 7-byte preamble
+        self.emac.cfg.modify(|_, w| w.ifg()._96()); // 96-bit iter-frame gap
+        self.emac.cfg.modify(|_, w| w.bl()._1024()); // 1024-bit back-off limit
+        self.emac.cfg.modify(|_, w| w.ps().set_bit()); // Set port select (req. for MII/RMII PHY interface)
+
+        // Set memory controller op mode
+        self.emac.dmaopmode.modify(|_, w| w.rsf().set_bit()); // RX store-and-forward
+        self.emac.dmaopmode.modify(|_, w| w.tsf().set_bit()); // TX store-and-forward
+        self.emac.dmaopmode.modify(|_, w| w.ttc()._32()); // TX threshold
+        self.emac.dmaopmode.modify(|_, w| w.rtc()._32()); // RX threshold
     }
 
-    async fn receive(data: &mut [u8]) {
+    async fn transmit(data: &[u8]) {}
 
-    }
+    async fn receive(data: &mut [u8]) {}
 }
 
 /// Choices of speed standard.
-#[allow(missing_docs)]
-pub enum Speed {
-    _10,
-    _100
-}
+// #[allow(missing_docs)]
+// pub enum Speed {
+//     _10,
+//     _100,
+// }
 
 /// Choices of preamble length in bytes.
-/// 
+///
 /// This is the number of alternating 0-1 bits transmitted at the start of each frame
 /// in order to synchronize clocks between the transmitter and receiver.
 #[allow(missing_docs)]
 pub enum PreambleLength {
     _3,
     _5,
-    _7
+    _7,
 }
 
 /// Choices of interframe gap length in bits.
-/// 
+///
 /// This is the duration of radio-silence used to signal the end of a transmission frame.
 #[allow(missing_docs)]
 pub enum InterFrameGap {
@@ -179,11 +249,11 @@ pub enum InterFrameGap {
     _72,
     _80,
     _88,
-    _96
+    _96,
 }
 
 /// Choices of back-off limit in bits.
-/// 
+///
 /// This is a cap on the rescheduling duration given by the exponential back-off algorithm
 /// in the event of repeated collisions during transmission.
 #[allow(missing_docs)]
@@ -191,11 +261,11 @@ pub enum BackOffLimit {
     _2,
     _8,
     _256,
-    _1024
+    _1024,
 }
 
 /// TX memory transfer threshold for memory controller
-/// 
+///
 /// This is the number of bytes in the buffer required to trigger a transfer.
 #[allow(missing_docs)]
 pub enum TXThresholdDMA {
@@ -206,18 +276,18 @@ pub enum TXThresholdDMA {
     _64,
     _128,
     _192,
-    _256
+    _256,
 }
 
 /// RX memory transfer threshold for memory controller
-/// 
+///
 /// This is the number of bytes in the buffer required to trigger a transfer.
 #[allow(missing_docs)]
 pub enum RXThresholdDMA {
     _32,
     _64,
     _96,
-    _128
+    _128,
 }
 
 /// RX memory transfer burst size in 32-bit words
@@ -232,7 +302,11 @@ pub enum BurstSizeDMA {
     _32,
 
     // Settings that do require 8x flag
-    _64,
-    _128,
-    _256
+    // The 8x flag must match both RX and TX burst size, which introduces complicated logic
+    // that is difficult to implement without introducing panic branches or unexpected behavior
+    // to resolve incompatible input combinations
+
+    // _64,
+    // _128,
+    // _256,
 }

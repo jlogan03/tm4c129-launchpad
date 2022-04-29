79497e9a326cc2aef7a4fb6cd7ea73d0306d15f5
James Logan <jlogan03@gmail.com>
2022-04-29 17:17:39 -0400

    move gitignore up. start building udpclient.

--- software/udpclient/src/main.rs
+++ software/udpclient/src/main.rs
@@ -1,3 +1,17 @@
+use std::net::UdpSocket;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = UdpSocket::bind("127.0.0.1:8053").unwrap();
+
+    // Receives a single datagram message on the socket. If `buf` is too small to hold
+    // the message, it will be cut off.
+    let mut buf = [0; 10];
+    loop {
+        match socket.recv_from(&mut buf) {
+            Ok((amt, src)) => {println!("{:?}", buf);},
+            _ => {}
+        }
+        
+        socket.send_to(&[0; 10], "10.0.0.2:8053").expect("couldn't send data");
+    }
 }

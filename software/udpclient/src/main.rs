7527f98746e667c7523342d26f1d7860824d66f8
James Logan <jlogan03@gmail.com>
2022-05-01 11:33:15 -0400

    update to properly bind 0.0.0.0 to allow socket to connect to dst addr

--- software/udpclient/src/main.rs
+++ software/udpclient/src/main.rs
@@ -4,13 +4,14 @@ use std::{thread, time::Duration};
 fn main() {
     // 10.0.0.1 is the gateway and *.2 is the DHCP server
     // 0.0.0.0 -> broadcast
-    let dst_addr = "0.0.0.0:8053";
+    let dst_addr = "10.0.0.229:8053";
 
-    // Bind a socket and tell it to listen for input from a specific address
+    // Bind a port
     let socket = UdpSocket::bind("0.0.0.0:8053").unwrap();
     socket
         .set_read_timeout(Some(Duration::from_nanos(1)))
         .unwrap();
+    // Tell the socket to listen for 
     let connected = match socket.connect(dst_addr) {
         Ok(_) => {
             println!("Socket connection success");
@@ -28,6 +29,8 @@ fn main() {
     let mut i = 0;
     loop {
         println!("{i} Loop Start");
+
+        // Receive all buffered frames
         if connected {
             while let Ok((amt, src)) = socket.recv_from(&mut buf) {
                 println!("{i} Received {amt} bytes from {src} : {:?}", unsafe {
@@ -38,13 +41,20 @@ fn main() {
             println!("{i} Skipping recv due to connection failure")
         }
 
+        // Send specifically to device
         match socket.send_to(b"greetings", dst_addr) {
+            Ok(_) => println!("{i} Data send success"),
+            Err(x) => println!("{i} Data send failure: {x:?}"),
+        }
+
+        // Broadcast
+        match socket.send_to(b"greetings", "0.0.0.0:8053") {
             Ok(_) => println!("{i} Data send success"),
             Err(x) => println!("{i} Data send failure: {x:?}"),
         }
 
         i += 1;
 
-        thread::sleep(Duration::from_millis(500));
+        thread::sleep(Duration::from_millis(250));
     }
 }

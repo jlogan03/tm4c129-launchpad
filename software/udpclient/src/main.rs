33a1c8dec52104faf01284453ae5c938f8bb59b6
James Logan <jlogan03@gmail.com>
2022-04-30 20:01:04 -0400

    add timeout limit on receive. get loopback message working.

--- software/udpclient/src/main.rs
+++ software/udpclient/src/main.rs
@@ -8,6 +8,9 @@ fn main() {
 
     // Bind a socket and tell it to listen for input from a specific address
     let socket = UdpSocket::bind("127.0.0.1:8053").unwrap();
+    socket
+        .set_read_timeout(Some(Duration::from_millis(10)))
+        .unwrap();
     let connected = match socket.connect(dst_addr) {
         Ok(_) => {
             println!("Socket connection success");
@@ -20,14 +23,14 @@ fn main() {
     };
 
     // Try to send and receive
-    let mut buf: [u8; 1500] = [0; 1500];
+    let mut buf: [u8; 1522] = [0; 1522];
     let mut i = 0;
     loop {
         println!("{i} Loop Start");
         if connected {
             match socket.recv_from(&mut buf) {
                 Ok((amt, src)) => {
-                    println!("Received {amt} bytes from {src} : {:?}", buf);
+                    println!("Received {amt} bytes from {src} : {:?}", unsafe{String::from_utf8_unchecked(buf[0..amt].to_vec())});
                 }
                 Err(x) => {
                     println!("{i} Receive error: {x:?}");
@@ -37,7 +40,7 @@ fn main() {
             println!("{i} Skipping recv due to connection failure")
         }
 
-        match socket.send_to(&[0; 10], dst_addr) {
+        match socket.send_to(b"greetings", dst_addr) {
             Ok(_) => println!("{i} Data send success"),
             Err(x) => println!("{i} Data send failure: {x:?}"),
         }

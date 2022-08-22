e6f0589f233ed05cf5f8453cb7449505072bf930
James Logan <jlogan03@gmail.com>
2022-08-21 20:21:23 -0400

    rework to store values to send to influx and forward them once per spam interval

--- software/udpclient/src/main.rs
+++ software/udpclient/src/main.rs
@@ -2,7 +2,7 @@ use std::net::UdpSocket;
 use std::time::Instant;
 use std::{thread, time::Duration};
 use chrono::Utc;
-use hyper::{Body, Method, Request, Uri, Client};
+use hyper::{Body, Method, Request, Client};
 use rpassword::prompt_password;
 
 const INFLUXDB_PORT: u16 = 8886;
@@ -14,17 +14,18 @@ struct Influxdb {
     pub token: String
 }
 
-// #[tokio::main]
-fn main() {
-    // // Set up influxdb
-    // let client = Client::new();
-    // let idb: Influxdb = Influxdb {
-    //     port: INFLUXDB_PORT,
-    //     bucket: "test",
-    //     org: "isthmus",
-    //     token: prompt_password("InfluxDB access token: ").unwrap()
-    // };
-    // let idb_uri = format!("http://localhost:{}/api/v2/write?org={}&bucket={}&precision=ns", idb.port, idb.org, idb.bucket);
+#[tokio::main]
+async fn main() {
+    // Set up influxdb
+    let client = Client::new();
+    let idb: Influxdb = Influxdb {
+        port: INFLUXDB_PORT,
+        bucket: "test",
+        org: "isthmus",
+        token: prompt_password("InfluxDB access token: ").unwrap()
+    };
+    let idb_uri = format!("http://localhost:{}/api/v2/write?org={}&bucket={}&precision=ns", idb.port, idb.org, idb.bucket);
+    let token_str = format!("Token {}", &idb.token);
 
     // 10.0.0.1 is the gateway and *.2 is the DHCP server
     // 0.0.0.0 -> broadcast
@@ -53,27 +54,27 @@ fn main() {
 
     thread::sleep(Duration::from_millis(1000));
 
+    // Metrics
+    let mut latencies: Vec<f64> = Vec::new();
+    let mut times: Vec<i64> = Vec::new();
+
     // Try to send and receive
     let mut buf: [u8; 1522] = [0; 1522];
     let mut i = 0;
     let mut sent: u64 = 0;
     let mut recvd: u64 = 0;
-    let min_timeout = Duration::from_micros(300).as_secs_f64();
-    let mut timeout = Duration::from_micros(300).as_secs_f64();
+    // let min_timeout = Duration::from_micros(300).as_secs_f64();
+    let mut timeout = Duration::from_micros(500).as_secs_f64();
     let spam_interval = Duration::from_millis(1000);
     let mut last_spam = Instant::now();
-    let start_of_loop = Instant::now();
-    let mut elapsed: u64;
     let mut rate: f64;
     let mut loss_rate: f64;
     let mut latency: f64;
     let mut mean_latency_us: f64 = 0.0;
     loop {
-        // Do some formatting out of the loop
-        // let token_str = format!("Token {}", &idb.token);
 
         // Send a unique message to the device
-        let msg = format!("greetings my dude. it is wednesday {i}");
+        let msg = format!("greetings, my dude. it is wednesday {i}");
         let msg_bytes = msg.as_bytes();
         match socket.send_to(&msg_bytes, dst_addr) {
             Ok(_) => {}
@@ -90,24 +91,9 @@ fn main() {
             } else if let Ok((amt, _)) = socket.recv_from(&mut buf) {
                 if &buf[..amt] == msg_bytes {
                     recvd += 1;
-                    // Keep running average of roundtrip latency
-                    mean_latency_us = ((recvd - 1) as f64) / (recvd as f64) * mean_latency_us
-                        + 1.0 / (recvd as f64) * latency * 1e6;
-                    timeout = (4.0 * mean_latency_us / 1e6).max(min_timeout);
-
-                    // Send to influxdb
-                    // let t = Utc::now().timestamp_nanos();
-                    // let latency_ns = (latency * 1e9) as u32;
-                    // let line = format!("board,units=s latency={latency} {t}");
-                    // let req = Request::builder()
-                    // .method(Method::POST)
-                    // .uri(&idb_uri)
-                    // .header("Authorization", &token_str)
-                    // .header("Content-Type", "text/plain; charset=utf-8")
-                    // .header("Accept", "application/json")
-                    // .body(Body::from(line)).unwrap();
-                    // let _ = client.request(req).await;
-                    // println!("{:?}", resp);
+                    // Store values to send to influxdb
+                    latencies.push(latency);
+                    times.push(Utc::now().timestamp_nanos());
 
                     break 'outer;
                 }
@@ -116,12 +102,47 @@ fn main() {
             }
         }
 
-        if last_spam.elapsed() > spam_interval {
-            last_spam = Instant::now();
-            elapsed = start_of_loop.elapsed().as_secs();
-            rate = (recvd as f64) / (elapsed as f64);
+        let elapsed = last_spam.elapsed();
+        if elapsed > spam_interval {
+            // Keep running average of roundtrip latency and drop rate
+            mean_latency_us = latencies.iter().sum::<f64>() / (latencies.len() as f64) * 1e6;
+            rate = (recvd as f64) / elapsed.as_secs_f64();
             loss_rate = ((sent - recvd) as f64) / (sent as f64) * 100.0;
             println!("{i} Sent: {sent} [packets], Received: {recvd} [packets], Elapsed: {elapsed:?} [s], Round-Trip Rate: {rate:.1} [packets/sec], Loss Rate: {loss_rate:.4} [percent], Mean Latency: {mean_latency_us:.0} [us]");
+            // Reset counters
+            sent = 0;
+            recvd = 0;
+
+
+            // Send stored values to influxdb and clear buffers
+            let mut lines: Vec<String> = Vec::new();
+            while times.len() >= 1 {
+                let t = times.pop().unwrap_or_default();
+                let l = latencies.pop().unwrap_or_default();
+                lines.push(format!("board,units=s latency={l} {t}"))
+            }
+            let req = Request::builder()
+            .method(Method::POST)
+            .uri(&idb_uri)
+            .header("Authorization", &token_str)
+            .header("Content-Type", "text/plain; charset=utf-8")
+            .header("Accept", "application/json")
+            .body(Body::from(lines.join("\n"))).unwrap();
+            // We need to block on this request to avoid clogging the wire while the test is in progress
+            // Check the response but only print if there was an error
+            let resp = client.request(req).await;
+            match resp {
+                Ok(x) => {
+                    match x.status().is_success() {
+                        true => {},
+                        false => println!("{:?}", x)
+                    }
+                }
+                Err(x) => {println!("{:?}", x);}
+            }
+
+            // Reset timer at end so that it doesn't absorb timing from database comms
+            last_spam = Instant::now();
         }
 
         i += 1;

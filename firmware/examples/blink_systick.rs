03293514b9e0ead4158a0de5ceb6719e2f0f82c1
James Logan <jlogan03@gmail.com>
2022-12-04 23:34:01 -0500

    remove unnecessary clearing of counter

--- firmware/examples/blink_systick.rs
+++ firmware/examples/blink_systick.rs
@@ -16,34 +16,12 @@ use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::digital::v2::*; // GPIO set high/low
 use embedded_hal::serial::Read as ReadHal;
 use tm4c129_launchpad::board::{clocks, safe, Board};
-use tm4c129_launchpad::startup::{Interrupt};
+use tm4c129_launchpad::startup::Interrupt;
 use tm4c129x_hal::gpio::GpioExt;
 use tm4c129x_hal::serial;
 use tm4c129x_hal::time::Bps;
 
 use irq::{handler, scope, scoped_interrupts};
-
-
-fn systick() {
-    use tm4c129x_hal::{gpio::GpioExt, serial, sysctl::SysctlExt, time::Bps, CorePeripherals};
-    let mut core_peripherals = unsafe { CorePeripherals::steal() };
-
-    core_peripherals.SYST.disable_interrupt();
-    core_peripherals.SYST.clear_current();
-
-    let p = unsafe { tm4c129x_hal::Peripherals::steal() };
-    let pins = p.GPIO_PORTF_AHB.split(&p.SYSCTL.constrain().power_control);
-
-    let mut delay = tm4c129x_hal::delay::Delay::new(core_peripherals.SYST, unsafe { &clocks() });
-    let mut led0 = pins.pf0.into_push_pull_output();
-    loop {
-        let _ = led0.set_high().unwrap_or_default();
-        delay.delay_ms(500u32);
-        let _ = led0.set_low().unwrap_or_default();
-        delay.delay_ms(500u32);
-    }
-}
-
 
 #[no_mangle]
 pub fn stellaris_main(mut board: Board) -> ! {
@@ -60,84 +38,86 @@ pub fn stellaris_main(mut board: Board) -> ! {
         &board.power_control,
     );
 
-    // let mut delay = tm4c129x_hal::delay::Delay::new(board.core_peripherals.SYST, clocks());
+    // Set up systick as static scheduling interrupt
+    //    Set tick timing
+    let us = 100; // How many us per tick?
+    let mut reload = us * (clocks().sysclk.0 / 1_000_000) + 1;  // Interrupt after this many clock cycles
+    //    Enable SysTick interrupt
+    board.core_peripherals.SYST.set_reload(reload);
+    board.core_peripherals.SYST.clear_current();
+    board.core_peripherals.SYST.enable_counter();
+    board.core_peripherals.SYST.enable_interrupt();
 
-    // Set up systick interrupt
-    //    Set timing duri
-    let us = 1000;
-    let mut rvr = us * (clocks().sysclk.0 / 1_000_000);
-    rvr += (us * ((clocks().sysclk.0 % 1_000_000) / 1_000)) / 1_000;
-    rvr += (us * (clocks().sysclk.0 % 1_000)) / 1_000_000;
+    writeln!(
+        uart,
+        "SysTick Blink {}",
+        board.core_peripherals.SYST.is_interrupt_enabled()
+    )
+    .unwrap_or_default();
 
-    &board.core_peripherals.SYST.set_reload(rvr);
-    &board.core_peripherals.SYST.clear_current();
-    &board.core_peripherals.SYST.enable_counter();
-    // let _ = &board.core_peripherals.ITM.
-    &board.core_peripherals.SYST.enable_interrupt();
+    let mut ticks: u64 = 0;
+    let mut loops: u8 = 0;
 
-    writeln!(uart, "SysTick Blink {}", &board.core_peripherals.SYST.is_interrupt_enabled()).unwrap_or_default();
+    handler!(
+        systick_handler = || {
+            ticks = ticks.wrapping_add(1);
 
-    let mut loops = 0;
+            if ticks % 100 == 0 {  // Cycle LEDs once every hundred systicks
+                loops = loops.wrapping_add(1);
 
-    // handler!(
-    //     systick = || {
-    //         // Spam serial
-    //         writeln!(uart, "Hello, world! Loops = {}", loops).unwrap_or_default();
-    //         while let Ok(ch) = uart.read() {
-    //             // Echo
-    //             writeln!(uart, "byte read {}", ch).unwrap_or_default();
-    //         }
+                // Spam serial
+                writeln!(uart, "Hello, world! Ticks = {}; Loops = {}", ticks, loops).unwrap_or_default();
+                while let Ok(ch) = uart.read() {
+                    // Echo
+                    writeln!(uart, "byte read {}", ch).unwrap_or_default();
+                }
 
-    //         loops = loops + 1;
+                if *(&(board.button0).is_low().unwrap_or_default()) {
+                    // Turn all the LEDs on
+                    let _ = &(board.led0).set_high().unwrap_or_default();
+                    let _ = &(board.led1).set_high().unwrap_or_default();
+                    let _ = &(board.led2).set_high().unwrap_or_default();
+                    let _ = &(board.led3).set_high().unwrap_or_default();
+                } else {
+                    let which_led = loops % 4;
 
-    //         if *(&(board.button0).is_low().unwrap_or_default()) {
-    //             // Turn all the LEDs on
-    //             let _ = &(board.led0).set_high().unwrap_or_default();
-    //             let _ = &(board.led1).set_high().unwrap_or_default();
-    //             let _ = &(board.led2).set_high().unwrap_or_default();
-    //             let _ = &(board.led3).set_high().unwrap_or_default();
-    //         } else {
-    //             let which_led = loops % 4;
+                    // Turn all the LEDs off
+                    let _ = &(board.led0).set_low().unwrap_or_default();
+                    let _ = &(board.led1).set_low().unwrap_or_default();
+                    let _ = &(board.led2).set_low().unwrap_or_default();
+                    let _ = &(board.led3).set_low().unwrap_or_default();
 
-    //             // Turn all the LEDs off
-    //             let _ = &(board.led0).set_low().unwrap_or_default();
-    //             let _ = &(board.led1).set_low().unwrap_or_default();
-    //             let _ = &(board.led2).set_low().unwrap_or_default();
-    //             let _ = &(board.led3).set_low().unwrap_or_default();
-
-    //             // Turn on just the selected LED
-    //             match which_led {
-    //                 0 => {
-    //                     let _ = &(board.led1).set_high().unwrap_or_default();
-    //                 }
-    //                 1 => {
-    //                     let _ = &(board.led1).set_high().unwrap_or_default();
-    //                 }
-    //                 2 => {
-    //                     let _ = &(board.led2).set_high().unwrap_or_default();
-    //                 }
-    //                 3 => {
-    //                     let _ = &(board.led3).set_high().unwrap_or_default();
-    //                 },
-    //                 _ => {}
-    //             }
-    //         }
-    //     }
-    // );
+                    // Turn on just the selected LED
+                    match which_led {
+                        0 => {
+                            let _ = &(board.led0).set_high();
+                        }
+                        1 => {
+                            let _ = &(board.led1).set_high();
+                        }
+                        2 => {
+                            let _ = &(board.led2).set_high();
+                        }
+                        3 => {
+                            let _ = &(board.led3).set_high();
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    );
 
     // Create a scope and register the systick interrupt handler.
-    handler!(systick_handler = || systick());
+    // handler!(systick_handler = || systick());
     scope(|s| {
         s.register(Interrupt::SysTick, systick_handler);
-
 
         loop {
             // Wait for interrupt
         }
     });
 
-    // Must not return
-    loop {};
-
-
+    // Main must not return
+    loop {}
 }
